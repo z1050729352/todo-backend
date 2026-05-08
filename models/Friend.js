45e3a2e942ae600d64bf35782762d89e4cd66664
z1050729352 <zbd25057431>
2026-05-08 19:44:34 +0800

    fix: 测试自动部署 22

--- models/Friend.js
+++ models/Friend.js
@@ -26,4 +26,4 @@ const friendSchema = new mongoose.Schema({
 friendSchema.index({ requester: 1, recipient: 1 }, { unique: true });
 
 module.exports = mongoose.model('Friend', friendSchema);
-// 测试一下宝塔自动部署 2
+// 测试一下宝塔自动部署 22
